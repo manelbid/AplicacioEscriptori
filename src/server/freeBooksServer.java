0ae09151f7f9044eafcbb51cac02d6d0b24caa77
Acer <Acer@DESKTOP-1UO4VBI>
2018-03-03 19:15:36 +0100

    Merge origin/Branch1FB
    Conflicts:
    	build.xml
    	nbproject/build-impl.xml
    	nbproject/genfiles.properties
    	nbproject/project.properties
    	nbproject/project.xml

--- src/server/freeBooksServer.java
+++ src/server/freeBooksServer.java
@@ -15,96 +15,89 @@ import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-
 
 /**
  *
  * @author ferranb
  */
 public class freeBooksServer {
+
     protected Socket socket;
     private String codiSessio = "";
-    
-    
-    
-    public void setSocket(Socket socket){
+
+    public void setSocket(Socket socket) {
         this.socket = socket;
     }
-    
-    public void executa(){
-        try{
+
+    public void executa() {
+        try {
             BufferedWriter escriptor;
             //Llegeix el missatge del client i l'analitza
-            try(BufferedReader lector = new BufferedReader(new InputStreamReader(socket.getInputStream()))){
+            try (BufferedReader lector = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
                 String request = lector.readLine();
-                
+
                 escriptor = new BufferedWriter(
                         new OutputStreamWriter(socket.getOutputStream()));
                 String resposta = "";
-                switch(request.split("-")[0]){
+                switch (request.split("-")[0]) {
                     case "userLogin":
                         resposta = userLogin(request);
-                        if (!resposta.equals("FAIL")){
+                        if (!resposta.equals("FAIL")) {
                             codiSessio = resposta;
                         }
+                        break;
                     case "nouLogin":
-                        resposta = addNewLogin(request);   
-                        
-                        
+                        resposta = addNewLogin(request);
+                        break;
                 }
                 escriptor.write(resposta);
                 escriptor.newLine();
-                escriptor.flush();                
+                escriptor.flush();
             }
-            
+
             //escriptor.close();
-            
-        } catch(Exception e){
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
-    
-    
-    private String userLogin(String dades) throws UtilitatPersistenciaException{
+
+    private String userLogin(String dades) throws UtilitatPersistenciaException {
         String result = "";
         GestorPersistencia gp = new GestorPersistencia("PantallaLoginUP");
         gp.obrir();
         List<Login> llista = gp.findUsersByNameAndPass(dades);
         gp.tancar();
-        result = llista.isEmpty()?"FAIL":"OK"+dades.split("-")[1]+new java.util.Date().toString();
+        result = llista.isEmpty() ? "FAIL" : "OK" + dades.split("-")[1] + new java.util.Date().toString();
         return result;
     }
-    
-    private String addNewLogin(String dades) throws UtilitatPersistenciaException {        
-        
+
+    private String addNewLogin(String dades) throws UtilitatPersistenciaException {
+
         String res = "FAIL";
-        GestorPersistencia gp = new GestorPersistencia("PantallaLoginPU");
+        GestorPersistencia gp = new GestorPersistencia("PantallaLoginUP");
         gp.obrir();
-        if(gp.findByName(dades)){
-            Login log = new Login(dades.split("-")[1],dades.split("-")[2],"Desktop",Boolean.FALSE);
+        if (gp.findByName(dades)) {
+            Login log = new Login(dades.split("-")[1], dades.split("-")[2], "Desktop", Boolean.FALSE);
             gp.inserirLogin(log);
             gp.gravaCanvis();
-            gp.tancar();            
+            gp.tancar();
             res = "OK";
         }
         gp.tancar();
         return res;
     }
-    
-    public static void main(String[]args) throws Exception{
+
+    public static void main(String[] args) throws Exception {
         int port = 9999;
         System.out.println("El server de FreeBooks està en execució");
         ServerSocket serverSocket = new ServerSocket(port);
         freeBooksServer fbs = new freeBooksServer();
-        while(true){
+        while (true) {
             Socket socket = serverSocket.accept();
             fbs.setSocket(socket);
             fbs.executa();
         }
-        
+
         //serverSocket.close();
     }
 }
